94e82de971ef92325d97e515bd4bc7e2819bd198
mahmoud mostageer <mmostageer67@gmail.com>
2026-07-09 22:46:56 +0300

    3379. Transformed Array

---
+++ array transfrmation.js/circular.array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var constructTransformedArray = function(nums) {
+    const n = nums.length;
+    const result = new Array(n);
+
+    for (let i = 0; i < n; i++) {
+        const targetIndex = ((i + nums[i]) % n + n) % n;
+        result[i] = nums[targetIndex];
+    }
+
+    return result;
+};
