94672ab3fdbaf2cb3419ef32988c25a903db4407
mahmoud mostageer <mmostageer67@gmail.com>
2026-07-29 05:02:20 +0300

    Add sortArrayByParityII problem solution

---
+++ array transfrmation.js/sortArrayByParityII.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(nums) {
+    const ans = new Array(nums.length);
+
+    let even = 0;
+    let odd = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0) {
+            ans[even] = nums[i];
+            even += 2;
+        } else {
+            ans[odd] = nums[i];
+            odd += 2;
+        }
+    }
+
+    return ans;
+};
